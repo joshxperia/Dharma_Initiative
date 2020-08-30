e7541c951652b82b9fe59710746bd51f47268e17
joshua <joshtrix8@gmail.com>
2020-08-31 02:40:37 +0300

    Update

--- src/com/company/ArrayStructures.java
+++ src/com/company/ArrayStructures.java
@@ -39,7 +39,44 @@ public void printArray(){
       return valueInArray;
   }
 
+public void deletIndex(int index){
+        if(index < arraySize){
+            for(int i = index; i<(arraySize-1); i++){
+                theArray[i] = theArray[i+1];
+            }
+            arraySize--;
+        }
 
+}
+  public void insertValue(int value){
+        if(arraySize<50){
+            theArray[arraySize] = value;
+            arraySize++;
+        }
+
+  }
+
+  public String linearSearchForValue(int value) {
+
+      boolean valueInArray = false;
+      String indexWithValue = "";
+      System.out.print("The values are shown bellow");
+      for (int m = 0; m < arraySize; m++) {
+          if (theArray[m] == value) {
+              valueInArray = true;
+              System.out.print(m + "");
+              indexWithValue += m + "";
+
+          }
+      }
+
+   if(!valueInArray){
+       indexWithValue ="none";
+       System.out.print(indexWithValue);
+   }
+       System.out.println();
+   return indexWithValue;
+  }
     public static void main(String[] args){
 
         ArrayStructures newArray = new ArrayStructures();
@@ -47,5 +84,10 @@ public void printArray(){
         newArray.printArray();
         System.out.println(newArray.getValueAtIndex(4));
         System.out.print(newArray.doesArrayContainThisValue(18));
+        newArray.deletIndex(2);
+        newArray.printArray();
+        newArray.insertValue(55);
+
+
     }
 }
